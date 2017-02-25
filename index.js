572ef7397a120e603477789ee24149ca94e80641
york yao <yaoao12306@outlook.com>
2017-02-25 21:30:53 +0800

    first version

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const minimist = require("minimist");
+
+const app = express();
+
+app.get("/", (request, response) => {
+    response.send("backend: first version");
+});
+
+const argv = minimist(process.argv.slice(2));
+const port = argv["p"] || 9001;
+
+app.listen(port, () => {
+    console.log(`server is listening: ${port}`);
+});
