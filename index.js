f0ba980815b6ab0197dc96fb0d5504d8bac18c5f
york yao <yaoao12306@outlook.com>
2017-02-25 21:33:49 +0800

    add host

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get("/", (request, response) => {
 const argv = minimist(process.argv.slice(2));
 const port = argv["p"] || 9001;
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`server is listening: ${port}`);
 });
